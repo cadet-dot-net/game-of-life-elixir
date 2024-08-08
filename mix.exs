9b666dd418ef7ae37ee4314a947abc379a3ab34a
cadet-dot-net <163072113+cadet-dot-net@users.noreply.github.com>
2024-08-08 14:16:11 +0100

    allow support for elixir 1.16

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule GameOfLife.MixProject do
     [
       app: :game_of_life,
       version: "0.1.0",
-      elixir: "~> 1.17",
+      elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
